c6a1207618d93b9ed843b9546d58534e275e9924
Jyotiprakash Ghorai <jyotiprakashghorai164@gmail.com>
2023-02-06 13:46:13 +0530

    Day 2 completd

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  int days = 30;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-      child: Container(child: Text("Welcode to my world")),
-    ));
+    return HomePage();
   }
 }
